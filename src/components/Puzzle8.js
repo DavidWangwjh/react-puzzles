62fdfc50577f74e665ff7b8d3e997ff1e6246e1c
DavidWangwjh <davidwang.wjh@gmail.com>
2025-05-16 17:27:53 -0700

    finished puzzle8

--- src/components/Puzzle8.js
+++ src/components/Puzzle8.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Puzzle8 = () => {
+
+  const [input, setInput] = useState('');
+  const [textArt, setTextArt] = useState('')
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    generateTextArt(e.target.value);
+  }
+
+  const generateTextArt = (input) => {
+    const chars = input.split('');
+    const textArtChars = [];
+    for (let i = 0; i < chars.length; i++) {
+      textArtChars.push(<span style={{ fontSize: `${10 + i * 2}px`, color: i%2 === 0? 'blue' : 'green'}}>{chars[i]}</span>)
+    }
+    setTextArt(textArtChars)
+  }
+
+  const clear = () => {
+    setInput('');
+    setTextArt('');
+  }
+
   return (
     <div>
-      <h3>Puzzle 8</h3>
-      <p>Coming soon...</p>
+      <p>{textArt}</p>
+      <input value={input} onChange={handleInput}/>
+      <button onClick={clear} style={{ margin: '1rem' }}>clear</button>
     </div>
   );
 };
